17a0e3633b0aafbe9d936050ab2007ca06184f95
MAwais <mawais54013@outlook.com>
2019-10-13 20:16:37 -0700

    Added Camera/Library Access

--- Progress Journal/CreateProgressUpdateViewController.swift
+++ Progress Journal/CreateProgressUpdateViewController.swift
@@ -8,25 +8,35 @@
 
 import UIKit
 
-class CreateProgressUpdateViewController: UIViewController {
+class CreateProgressUpdateViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
+    
+    var pickerController = UIImagePickerController()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         addButton.layer.cornerRadius = addButton.frame.size.height / 2
+        
+        pickerController.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
     }
     
     @IBAction func existingPhotoTapped(_ sender: Any) {
-        
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
-        
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
     }
     
     @IBAction func addTapped(_ sender: Any) {
